d89d35b531a67a1b63d2c916c7b0b0856871f995
Coury Richards <2702992+couryrr@users.noreply.github.com>
2026-02-28 14:00:18 -0500

    Added chunking and made the image much larger

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"image/color"
+	"math"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
@@ -17,10 +18,13 @@ var (
 	ScreenWidth  float32 = DefaultScreenWidth
 	ScreenHeight float32 = DefaultScreenHeight
 
-	tileSize      float32 = 2
-	playerSize    float32 = 2
-	playerSpeed   float32 = 100.0
-	TerrainColors         = []TerrainLevel{
+	chunksToRender float32 = 3
+	chunkSize      float32 = 15
+	chunkWorldSize float32 = chunkSize * tileSize
+	tileSize       float32 = 5
+	playerSize     float32 = 2
+	playerSpeed    float32 = 100.0
+	TerrainColors          = []TerrainLevel{
 		{0.30, rl.NewColor(10, 50, 100, 255)},   // Deep Water
 		{0.40, rl.NewColor(30, 100, 160, 255)},  // Shallow Water
 		{0.45, rl.NewColor(210, 190, 140, 255)}, // Sand
@@ -153,7 +157,7 @@ func main() {
 	defer rl.CloseWindow()
 	rl.SetTargetFPS(60)
 
-	perlin := rl.GenImagePerlinNoise(int(ScreenWidth)*2, int(ScreenHeight)*2, 0, 0, 10)
+	perlin := rl.GenImagePerlinNoise(int(ScreenWidth)*12, int(ScreenHeight)*12, 0, 0, 10)
 	defer rl.UnloadImage(perlin)
 
 	texture := rl.LoadRenderTexture(int32(VirtualWidth), int32(VirtualHeight))
@@ -174,11 +178,32 @@ func main() {
 		rl.ClearBackground(rl.White)
 		gameCamera.Camera.Target = rl.NewVector2(player.Position.X+player.Size.X/2, player.Position.Y+player.Size.Y/2)
 		rl.BeginMode2D(*gameCamera.Camera)
-		for x := int32(0); x < int32(ScreenWidth); x += int32(tileSize) {
-			for y := int32(0); y < int32(ScreenHeight); y += int32(tileSize) {
-				rl.DrawRectangle(x, y, int32(tileSize), int32(tileSize), DetermineTile(x, y, perlin))
+
+		chunkX := float32(math.Floor(float64(player.Position.X / chunkWorldSize)))
+		chunkY := float32(math.Floor(float64(player.Position.Y / chunkWorldSize)))
+		for dx := float32(-chunksToRender); dx <= chunksToRender; dx++ {
+			for dy := float32(-chunksToRender); dy <= chunksToRender; dy++ {
+				for x := float32(0); x < chunkSize; x++ {
+					for y := float32(0); y < chunkSize; y++ {
+						worldX := (chunkX+dx)*chunkWorldSize + x*tileSize
+						worldY := (chunkY+dy)*chunkWorldSize + y*tileSize
+						rl.DrawRectangleRec(rl.NewRectangle(worldX, worldY, tileSize, tileSize), DetermineTile(worldX, worldY, perlin))
+					}
+				}
+				worldRX := float32(chunkX+dx) * chunkWorldSize
+				worldRY := float32(chunkY+dy) * chunkWorldSize
+				rl.DrawRectangleLinesEx(rl.NewRectangle(worldRX, worldRY, chunkWorldSize, chunkWorldSize), 1, rl.Green)
 			}
 		}
+
+		// TODO: Fix these loops seems unneeded... guard mix/max of width/height...
+		// for x := float32(0); x < ScreenWidth; x += tileSize {
+		// 	rl.DrawLine(int32(x), int32(0), int32(x), int32(ScreenHeight), rl.Black)
+		// }
+		// for y := float32(0); y < ScreenHeight; y += tileSize {
+		// 	rl.DrawLine(int32(0), int32(y), int32(ScreenWidth), int32(y), rl.Black)
+		// }
+		
 		for _, mark := range marks {
 			rl.DrawRectangleLinesEx(rl.NewRectangle(mark.X, mark.Y, float32(tileSize), float32(tileSize)), 1, rl.White)
 		}
@@ -211,14 +236,17 @@ func HandleInput(player *Player, screenSetting *ScreenSetting, gameCamera *GameC
 		gameCamera.Camera.Rotation = 0
 	}
 	if rl.IsKeyPressed(rl.KeyTab) {
-		if gameCamera.CameraMode == CameraModePlanning {
+		switch gameCamera.CameraMode {
+		case CameraModePlanning:
 			gameCamera.ChangeMode(CameraModePlayer)
 			gameCamera.Camera.Zoom = 1.0
-		} else if gameCamera.CameraMode == CameraModePlayer {
+		case CameraModePlayer:
 			gameCamera.ChangeMode(CameraModePlanning)
 			gameCamera.Camera.Zoom = 0.5
+		default:
+			gameCamera.ChangeMode(CameraModePlayer)
+			gameCamera.Camera.Zoom = 1.0
 		}
-
 	}
 
 	if rl.IsMouseButtonPressed(rl.MouseButtonLeft) {
@@ -266,8 +294,16 @@ func HandleInput(player *Player, screenSetting *ScreenSetting, gameCamera *GameC
 	player.Position.Y += rotated.Y * playerSpeed * delta
 }
 
-func DetermineTile(x, y int32, perlin *rl.Image) color.RGBA {
-	n := float32(rl.GetImageColor(*perlin, x, y).R) / 255
+func DetermineTile(x, y float32, perlin *rl.Image) color.RGBA {
+	px := int32(x/tileSize) % perlin.Width
+	py := int32(y/tileSize) % perlin.Height
+	if px < 0 {
+		px += perlin.Width
+	}
+	if py < 0 {
+		py += perlin.Height
+	}
+	n := float32(rl.GetImageColor(*perlin, px, py).R) / 255.0
 	for _, t := range TerrainColors {
 		if n <= t.Threshold {
 			return t.Color
